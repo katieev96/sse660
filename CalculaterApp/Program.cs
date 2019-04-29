adba1c6df1f76c6e9712d9ff287ef4207dd1cd11
Katie Elizabeth Vick <10608995@live.mercer.edu>
2019-04-29 18:50:10 -0400

    Add project files.

---
+++ CalculaterApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculaterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
